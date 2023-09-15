38bc0aed6ea5a570a47ec8c303359f70b164ac9a
R1ch1no <richard.kurnava@gmail.com>
2023-09-15 18:47:03 +0200

    phoneshit

--- cpp00/ex01/PhoneBook.cpp
+++ cpp00/ex01/PhoneBook.cpp
@@ -6,19 +6,26 @@
 /*   By: rkurnava <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 18:49:33 by rkurnava          #+#    #+#             */
-/*   Updated: 2023/09/15 17:43:00 by rkurnava         ###   ########.fr       */
+/*   Updated: 2023/09/15 18:41:21 by rkurnava         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.class.hpp"
 #include <iostream>
 
-void	PrintSearch(std::string str, int index)
+void	PrintSearch(std::string str)
 {
 	int	i;
+	int	len;
 
 	i = 0;
-	index + 1;
+	len = str.length();
+	while (i < 9 - len)
+	{
+		std::cout << std::right << ' ';
+		i++;
+	}
+	i = 0;
 	while (i < 9 && str[i])
 	{
 		std::cout << std::right << str[i];
@@ -26,64 +33,63 @@ void	PrintSearch(std::string str, int index)
 	}
 	if (str[i] && i == 9)
 		std::cout << std::right << '.';
-	else if (i != 10)
-	{
-		while (i <= 10)
-		{
-			std::cout << std::right << ' ';
-			i++;
-		}
-	}
 }
 
 void	Search(Phonebook *book, int *num)
 {
 	int	index;
+	int	limit;
 
 	index = 0;
-	while (index < *num)
+	limit = *num;
+	if (limit > 8)
+		limit = 8;
+	while (index < limit)
 	{
-		std::cout << std::right << index + 1 << "         |";
-		PrintSearch(book->contact[index].FirstName, index);
+		std::cout << std::right << "         " << index + 1 << "|";
+		PrintSearch(book->contact[index].FirstName);
 		std::cout << std::right << "|";
-		PrintSearch(book->contact[index].LastName, index);
+		PrintSearch(book->contact[index].LastName);
 		std::cout << std::right << "|";
-		PrintSearch(book->contact[index].Nickname, index);
+		PrintSearch(book->contact[index].Nickname);
 		std::cout << std::right << "|";
-		PrintSearch(book->contact[index].PhoneNumber, index);
+		PrintSearch(book->contact[index].PhoneNumber);
 		std::cout << std::right << "|";
-		PrintSearch(book->contact[index].DarkestSecret, index);
-		std::cout << std::right << '\n';
+		PrintSearch(book->contact[index].DarkestSecret);
+		std::cout << std::right << "|";
+		std::cout << '\n';
 		index++;
 	}
 }
 
 void	Add(int *num, Phonebook *book)
 {
-	if (*num >= 8 || *num < 0)
-		*num = 7;
+	int	person;
+
+	person = *num % 8;
 	std::cout << "First name : ";
-	getline(std::cin, book->contact[*num].FirstName);
+	getline(std::cin, book->contact[person].FirstName);
 	std::cout << "Last name : ";
-	getline(std::cin, book->contact[*num].LastName);
+	getline(std::cin, book->contact[person].LastName);
 	std::cout << "Nickname : ";
-	getline(std::cin, book->contact[*num].Nickname);
+	getline(std::cin, book->contact[person].Nickname);
 	std::cout << "Phone number : ";
-	getline(std::cin, book->contact[*num].PhoneNumber);
+	getline(std::cin, book->contact[person].PhoneNumber);
 	std::cout << "Darkest secret : ";
-	getline(std::cin, book->contact[*num].DarkestSecret);
+	getline(std::cin, book->contact[person].DarkestSecret);
 	*num += 1;
 }
 
 int	main(int argc, char **argv)
 {
-	int num = 0;
+	int			num;
+	Phonebook	book;
+
+	num = 0;
 	std::string buffer;
 	std::string add = "ADD";
 	std::string search = "SEARCH";
 	std::string end = "EXIT";
-	Phonebook book;
-
 	if (argc != 1)
 	{
 		std::cout << "Execute program only, no arguments!" << std::endl;
@@ -97,7 +103,7 @@ int	main(int argc, char **argv)
 			buffer = " ";
 			getline(std::cin, buffer);
 			if (buffer == "\0")
-				continue;
+				continue ;
 			else if (buffer == end)
 				return (0);
 			else if (buffer == add)
