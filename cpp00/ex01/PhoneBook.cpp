310c9a0ef1c38a101c1c7044fdf97a2a76ba1e33
R1ch1no <richard.kurnava@gmail.com>
2023-09-18 14:31:42 +0200

    phonebook done

--- cpp00/ex01/PhoneBook.cpp
+++ cpp00/ex01/PhoneBook.cpp
@@ -6,11 +6,96 @@
 /*   By: rkurnava <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 18:49:33 by rkurnava          #+#    #+#             */
-/*   Updated: 2023/09/18 12:58:23 by rkurnava         ###   ########.fr       */
+/*   Updated: 2023/09/18 14:27:06 by rkurnava         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Phonebook.h"
+
+void	PrintSearch(std::string str)
+{
+	char	buffer[10];
+	int		i;
+	int		j;
+	int		len;
+
+	i = 0;
+	j = 0;
+	len = str.length();
+	while (i < 10 && i < 10 - len)
+	{
+		buffer[i] = ' ';
+		i++;
+	}
+	while (i < 10 && str[j])
+	{
+		buffer[i] = str[j];
+		i++;
+		j++;
+	}
+	if (len > 10)
+	{
+		if (str[10])
+			buffer[9] = '.';
+	}
+	std::cout << std::right << std::setw(10) << buffer << "|";
+}
+
+void	PrintContact(Phonebook *book, int contact)
+{
+	std::cout << "First name : " << book->contact[contact].FirstName << std::endl;
+	std::cout << "Last name : " << book->contact[contact].LastName << std::endl;
+	std::cout << "Nickname : " << book->contact[contact].Nickname << std::endl;
+	std::cout << "Phone number : " << book->contact[contact].PhoneNumber << std::endl;
+	std::cout << "Darkest secret : " << book->contact[contact].DarkestSecret << std::endl;
+}
+
+int	contactInfo(Phonebook *book, int limit)
+{
+	char	buffer[256];
+	int		contact;
+
+	std::string collector;
+	std::cout << "Who's information do you wish to see ? (index) : ";
+	std::cin >> buffer;
+	contact = 0;
+	contact = atoi(buffer);
+	getline(std::cin, collector);
+	if (contact <= 0 || contact > limit)
+	{
+		std::cout << "No contact found under this index! Try again : ";
+		std::cin >> buffer;
+		contact = atoi(buffer);
+		getline(std::cin, collector);
+	}
+	if (contact <= 0 || contact > limit)
+		std::cout << "No contact found under this index! Leaving search" << std::endl;
+	else
+		return (contact -1);
+	return (-1);
+}
+
+void	PrintBottom(void)
+{
+	std::cout << std::right << std::setw(11) << " -----------";
+	std::cout << std::right << std::setw(10) << "-----------";
+	std::cout << std::right << std::setw(10) << "-----------";
+	std::cout << std::right << std::setw(9) << "---------- " << std::endl;
+}
+
+void	PrintBody(Phonebook *book, int index, int limit)
+{
+	while (index < limit)
+	{
+		std::cout << std::right << std::setw(10) << "|         " << index
+			+ 1 << std::setw(1) << "|";
+		PrintSearch(book->contact[index].FirstName);
+		PrintSearch(book->contact[index].LastName);
+		PrintSearch(book->contact[index].Nickname);
+		std::cout << "\n";
+		index++;
+	}
+}
 
 void	PrintHead(void)
 {
@@ -28,73 +113,13 @@ void	PrintHead(void)
 	std::cout << std::right << std::setw(9) << "---------- " << std::endl;
 }
 
-void	PrintSearch(std::string str)
-{
-	char	buffer[10];
-	int		i;
-	int		j;
-	int		len;
-
-	i = 0;
-	j = 0;
-	len = str.length();
-	while (i < 10 && i < 10 - len)
-	{
-		buffer[i] = '.';
-		i++;
-	}
-	while (i < 10 && str[j])
-	{
-		buffer[i] = str[j];
-		i++;
-		j++;
-	}
-	if (len > 10)
-	{
-		if (str[10])
-			buffer[9] = '.';
-	}
-	std::cout << std::right << std::setw(10) << buffer << "|";
-}
-
-void	PrintBody(Phonebook *book, int index, int limit)
-{
-	while (index < limit)
-	{
-		std::cout << std::right << std::setw(10) << "|         " << index
-			+ 1 << std::setw(1) << "|";
-		PrintSearch(book->contact[index].FirstName);
-		PrintSearch(book->contact[index].LastName);
-		PrintSearch(book->contact[index].Nickname);
-		std::cout << "\n";
-		index++;
-	}
-}
-void	PrintBottom(void)
-{
-	std::cout << std::right << std::setw(11) << " -----------";
-	std::cout << std::right << std::setw(10) << "-----------";
-	std::cout << std::right << std::setw(10) << "-----------";
-	std::cout << std::right << std::setw(9) << "---------- " << std::endl;
-}
-
-void	PrintContact(Phonebook *book, int contact)
-{
-	std::cout << "First name : " << book->contact[contact].FirstName << std::endl;
-	std::cout << "Last name : " << book->contact[contact].LastName << std::endl;
-	std::cout << "Nickname : " << book->contact[contact].Nickname << std::endl;
-	std::cout << "Phone number : " << book->contact[contact].PhoneNumber << std::endl;
-	std::cout << "Darkest secret : " << book->contact[contact].DarkestSecret << std::endl;
-}
-
 void	Search(Phonebook *book, int *num)
 {
-	char	*buffer;
-	int		index;
-	int		limit;
-	int		contact;
+	int	contact;
+	int	index;
+	int	limit;
 
-	std::string collector[100];
+	std::string collector;
 	index = 0;
 	limit = *num;
 	if (limit > 8)
@@ -104,26 +129,22 @@ void	Search(Phonebook *book, int *num)
 	PrintHead();
 	PrintBody(book, index, limit);
 	PrintBottom();
-	std::cout << "Who's information do you wish to see ? (index) : ";
-	std::cin >> buffer;
-	getline(std::cin, collector);
-	contact = 0;
-	contact = atoi(buffer);
-	contact--;
-	if (contact < 0 || contact > limit)
-	{
-		std::cout << "No contact found under this index! Try again : ";
-		std::cin >> buffer;
-		getline(std::cin, collector);
-		contact = atoi(buffer);
-		contact--;
-	}
-	if (contact < 0 || contact > limit)
-	{
-		std::cout << "No contact found under this index! Leaving search" << std::endl;
+	contact = contactInfo(book, limit);
+	if (contact == -1)
 		return ;
-	}
 	PrintContact(book, contact);
+}
+
+int	isEmpty(std::string str)
+{
+	if (str.length() == 0)
+	{
+		std::cout << "Invalid contact\n"
+					<< "No contact field can be empty" << std::endl;
+		return (1);
+	}
+	else
+		return (0);
 }
 
 void	Add(int *num, Phonebook *book)
@@ -133,16 +154,27 @@ void	Add(int *num, Phonebook *book)
 	person = *num % 8;
 	std::cout << "First name : ";
 	getline(std::cin, book->contact[person].FirstName);
+	if (isEmpty(book->contact[person].FirstName))
+		return ;
 	std::cout << "Last name : ";
 	getline(std::cin, book->contact[person].LastName);
+	if (isEmpty(book->contact[person].LastName))
+		return ;
 	std::cout << "Nickname : ";
 	getline(std::cin, book->contact[person].Nickname);
+	if (isEmpty(book->contact[person].Nickname))
+		return ;
 	std::cout << "Phone number : ";
 	getline(std::cin, book->contact[person].PhoneNumber);
+	if (isEmpty(book->contact[person].PhoneNumber))
+		return ;
 	std::cout << "Darkest secret : ";
 	getline(std::cin, book->contact[person].DarkestSecret);
+	if (isEmpty(book->contact[person].DarkestSecret))
+		return ;
 	*num += 1;
 }
+
 
 int	main(int argc, char **argv)
 {
@@ -175,7 +207,10 @@ int	main(int argc, char **argv)
 			else if (buffer == search)
 				Search(&book, &num);
 			else
-				std::cout << "Operation not recognised, try again." << std ::endl;
+			{
+				std::cout << "Operation not recognised";
+				std::cout << ",try again." << std ::endl;
+			}
 		}
 	}
 	return (0);
