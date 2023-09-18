c657851d92e3f320cfb55d00f2f1d5892155493b
R1ch1no <richard.kurnava@gmail.com>
2023-09-18 13:39:49 +0200

    improvements

--- cpp00/ex01/PhoneBook.cpp
+++ cpp00/ex01/PhoneBook.cpp
@@ -6,80 +6,124 @@
 /*   By: rkurnava <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 18:49:33 by rkurnava          #+#    #+#             */
-/*   Updated: 2023/09/15 19:49:50 by rkurnava         ###   ########.fr       */
+/*   Updated: 2023/09/18 12:58:23 by rkurnava         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "PhoneBook.class.hpp"
-#include <iomanip>
-#include <iostream>
+#include "Phonebook.h"
+
+void	PrintHead(void)
+{
+	std::cout << std::right << std::setw(11) << " -----------";
+	std::cout << std::right << std::setw(10) << "-----------";
+	std::cout << std::right << std::setw(10) << "-----------";
+	std::cout << std::right << std::setw(9) << "---------- " << std::endl;
+	std::cout << std::right << std::setw(11) << "|     Index|";
+	std::cout << std::right << std::setw(11) << "First Name|";
+	std::cout << std::right << std::setw(11) << " Last Name|";
+	std::cout << std::right << std::setw(11) << "  Nickname|" << std::endl;
+	std::cout << std::right << std::setw(11) << " -----------";
+	std::cout << std::right << std::setw(10) << "-----------";
+	std::cout << std::right << std::setw(10) << "-----------";
+	std::cout << std::right << std::setw(9) << "---------- " << std::endl;
+}
 
 void	PrintSearch(std::string str)
 {
-	int	i;
-	int	len;
+	char	buffer[10];
+	int		i;
+	int		j;
+	int		len;
 
 	i = 0;
+	j = 0;
 	len = str.length();
-	while (i < 9 - len)
+	while (i < 10 && i < 10 - len)
 	{
-		std::cout << std::right << std::setw(1) << ' ';
+		buffer[i] = '.';
 		i++;
 	}
-	i = 0;
-	while (i < 9 && str[i])
+	while (i < 10 && str[j])
 	{
-		std::cout << std::right << std::setw(1) << str[i];
+		buffer[i] = str[j];
 		i++;
+		j++;
 	}
-	if (str[i] && i == 9)
-		std::cout << std::right << std::setw(1) << '.';
+	if (len > 10)
+	{
+		if (str[10])
+			buffer[9] = '.';
+	}
+	std::cout << std::right << std::setw(10) << buffer << "|";
 }
 
-void	PrintContact(Phonebook *book)
+void	PrintBody(Phonebook *book, int index, int limit)
 {
-	
+	while (index < limit)
+	{
+		std::cout << std::right << std::setw(10) << "|         " << index
+			+ 1 << std::setw(1) << "|";
+		PrintSearch(book->contact[index].FirstName);
+		PrintSearch(book->contact[index].LastName);
+		PrintSearch(book->contact[index].Nickname);
+		std::cout << "\n";
+		index++;
+	}
+}
+void	PrintBottom(void)
+{
+	std::cout << std::right << std::setw(11) << " -----------";
+	std::cout << std::right << std::setw(10) << "-----------";
+	std::cout << std::right << std::setw(10) << "-----------";
+	std::cout << std::right << std::setw(9) << "---------- " << std::endl;
+}
+
+void	PrintContact(Phonebook *book, int contact)
+{
+	std::cout << "First name : " << book->contact[contact].FirstName << std::endl;
+	std::cout << "Last name : " << book->contact[contact].LastName << std::endl;
+	std::cout << "Nickname : " << book->contact[contact].Nickname << std::endl;
+	std::cout << "Phone number : " << book->contact[contact].PhoneNumber << std::endl;
+	std::cout << "Darkest secret : " << book->contact[contact].DarkestSecret << std::endl;
 }
 
 void	Search(Phonebook *book, int *num)
 {
-	int	index;
-	int	limit;
+	char	*buffer;
+	int		index;
+	int		limit;
+	int		contact;
 
+	std::string collector[100];
 	index = 0;
 	limit = *num;
 	if (limit > 8)
 		limit = 8;
-	while (index < limit)
+	if (limit == 0)
+		return ;
+	PrintHead();
+	PrintBody(book, index, limit);
+	PrintBottom();
+	std::cout << "Who's information do you wish to see ? (index) : ";
+	std::cin >> buffer;
+	getline(std::cin, collector);
+	contact = 0;
+	contact = atoi(buffer);
+	contact--;
+	if (contact < 0 || contact > limit)
 	{
-		std::cout << std::right << std::setw(11) << " -----------";
-		std::cout << std::right << std::setw(10) << "-----------";
-		std::cout << std::right << std::setw(10) << "-----------";
-		std::cout << std::right << std::setw(9) << "---------- " << std::endl;
-		std::cout << std::right << std::setw(11) << "|     Index|";
-		std::cout << std::right << std::setw(11) << "First Name|";
-		std::cout << std::right << std::setw(11) << " Last Name|";
-		std::cout << std::right << std::setw(11) << "  Nickname|" << std::endl;
-		std::cout << std::right << std::setw(11) << " -----------";
-		std::cout << std::right << std::setw(10) << "-----------";
-		std::cout << std::right << std::setw(10) << "-----------";
-		std::cout << std::right << std::setw(9) << "---------- " << std::endl;
-		std::cout << std::right << std::setw(10) << "|         " << index
-			+ 1 << "|";
-		PrintSearch(book->contact[index].FirstName);
-		std::cout << std::right << "|";
-		PrintSearch(book->contact[index].LastName);
-		std::cout << std::right << "|";
-		PrintSearch(book->contact[index].Nickname);
-		std::cout << std::right << "|";
-		std::cout << "\n";
-		index++;
+		std::cout << "No contact found under this index! Try again : ";
+		std::cin >> buffer;
+		getline(std::cin, collector);
+		contact = atoi(buffer);
+		contact--;
 	}
-		std::cout << std::right << std::setw(11) << " -----------";
-		std::cout << std::right << std::setw(10) << "-----------";
-		std::cout << std::right << std::setw(10) << "-----------";
-		std::cout << std::right << std::setw(9) << "---------- " << std::endl;
-		PrintContact(book);
+	if (contact < 0 || contact > limit)
+	{
+		std::cout << "No contact found under this index! Leaving search" << std::endl;
+		return ;
+	}
+	PrintContact(book, contact);
 }
 
 void	Add(int *num, Phonebook *book)
@@ -130,6 +174,8 @@ int	main(int argc, char **argv)
 				Add(&num, &book);
 			else if (buffer == search)
 				Search(&book, &num);
+			else
+				std::cout << "Operation not recognised, try again." << std ::endl;
 		}
 	}
 	return (0);
---
+++ cpp00/ex01/Phonebook.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Phonebook.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rkurnava <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/15 20:39:42 by rkurnava          #+#    #+#             */
+/*   Updated: 2023/09/15 20:53:07 by rkurnava         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_H
+# define PHONEBOOH_H
+
+# include "PhoneBook.class.hpp"
+# include <cstdlib>
+# include <iomanip>
+# include <iostream>
+
+#endif
